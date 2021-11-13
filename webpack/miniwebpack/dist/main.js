5e5ef111d2a6c47781e8106399316fcedd4d168a
sunven <604164227@qq.com>
2021-11-13 15:05:18 +0800

    add notes

--- webpack/miniwebpack/dist/main.js
+++ webpack/miniwebpack/dist/main.js
@@ -1,13 +1,29 @@
-;(function(graph){
-        function require(module){
-            function otherRequire(relativePath){
-                return require(graph[module].dependencies[relativePath])
-            }
-            var exports = {}
-            ;(function(require,exports,code){
-                eval(code)
-            })(otherRequire,exports,graph[module].code)
-            return exports
-        }
-        require('./src/index.js')
-    })({"./src/index.js":{"dependencies":{"./a":"./src/a.js","./b":"./src/b.js"},"code":"\"use strict\";\n\nvar _a = require(\"./a\");\n\nvar _b = require(\"./b\");\n\nconsole.log('index.js', _a.str, _b.str);"},"./src/a.js":{"dependencies":{},"code":"\"use strict\";\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.str = void 0;\nvar str = 'a.js';\nexports.str = str;"},"./src/b.js":{"dependencies":{},"code":"\"use strict\";\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.str = void 0;\nvar str = 'b.js';\nexports.str = str;"}})
+;(function (graph) {
+  //自定义require
+  function require(module) {
+    function otherRequire(relativePath) {
+      return require(graph[module].dependencies[relativePath])
+    }
+    var exports = {}
+    ;(function (require, exports, code) {
+      //动态执行代码
+      eval(code)
+    })(otherRequire, exports, graph[module].code)
+    return exports
+  }
+  //先获取入口文件
+  require('./src/index.js')
+})({
+  './src/index.js': {
+    dependencies: { './a': './src/a.js', './b': './src/b.js' },
+    code: '"use strict";\n\nvar _a = require("./a");\n\nvar _b = require("./b");\n\nconsole.log(\'index.js\', _a.str, _b.str);',
+  },
+  './src/a.js': {
+    dependencies: {},
+    code: '"use strict";\n\nObject.defineProperty(exports, "__esModule", {\n  value: true\n});\nexports.str = void 0;\nvar str = \'a.js\';\nexports.str = str;',
+  },
+  './src/b.js': {
+    dependencies: {},
+    code: '"use strict";\n\nObject.defineProperty(exports, "__esModule", {\n  value: true\n});\nexports.str = void 0;\nvar str = \'b.js\';\nexports.str = str;',
+  },
+})
--- webpack/miniwebpack/lib/miniwebpack.js
+++ webpack/miniwebpack/lib/miniwebpack.js
@@ -1,63 +1,83 @@
+//babel使用的JavaScript解析器，得到ast
 const parser = require('@babel/parser')
+//遍历ast，解析出依赖
 const traverse = require('@babel/traverse').default
 const { transformFromAst } = require('@babel/core')
 const path = require('path')
 const fs = require('fs')
 class MiniWebpack {
   constructor(options) {
+    //入口
     this.entry = options.entry
+    //出口
     this.output = options.output
+    //模块
     this.modules = []
   }
   run() {
+    //解析
     const info = this.parse(this.entry)
     this.modules.push(info)
     for (let i = 0; i < this.modules.length; i++) {
       const { dependencies } = this.modules[i]
       if (dependencies) {
+        //遍历依赖，解析依赖
         for (let j in dependencies) {
           this.modules.push(this.parse(dependencies[j]))
         }
       }
     }
+    //modules中就保存了所有的模块，并且知道每个模块依赖那些模块
     const obj = {}
     this.modules.forEach(item => {
       obj[item.entryFile] = { dependencies: item.dependencies, code: item.code }
     })
+    // 生成出口文件
     this.file(obj)
   }
   file(code) {
     const filepath = path.join(this.output.path, this.output.filename)
+    //转为字符串
     const newcode = JSON.stringify(code)
     const bundle = `;(function(graph){
+        //自定义require
         function require(module){
             function otherRequire(relativePath){
                 return require(graph[module].dependencies[relativePath])
             }
             var exports = {}
             ;(function(require,exports,code){
+                //动态执行代码
                 eval(code)
             })(otherRequire,exports,graph[module].code)
             return exports
         }
+        //先获取入口文件
         require('${this.entry}')
     })(${newcode})`
+    //写到指定的出口文件
     fs.writeFileSync(filepath, bundle)
   }
+  //解析入口文件
   parse(entryFile) {
+    //读取文件
     const content = fs.readFileSync(entryFile).toString()
+    //转化为ast
     const ast = parser.parse(content, {
       sourceType: 'module',
     })
 
     const dependencies = {}
+    //遍历ast,解析出依赖那些模块
     traverse(ast, {
       ImportDeclaration: ({ node }) => {
+        // 将依赖收集到dependencies
         dependencies[node.source.value] =
           './' + path.join(path.dirname(entryFile), node.source.value) + '.js'
       },
     })
-
+    // 从ast转换出代码,
+    // 允许您使用最新的 JavaScript，而无需对目标环境需要哪些语法转换（以及可选的浏览器 polyfill）进行微观管理
     const { code } = transformFromAst(ast, null, {
       presets: ['@babel/preset-env'],
     })
